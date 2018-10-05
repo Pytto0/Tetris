38dfa95a2b43fc601a70feb67ec3909dc71da902
Pytto0 <43273350+Pytto0@users.noreply.github.com>
2018-10-05 11:13:45 +0200

    Nieuwste update

--- TetrisTemplate/TetrisGame.cs
+++ TetrisTemplate/TetrisGame.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using System.Collections;
+using System.Diagnostics;
 
 class TetrisGame : Game
 {
@@ -11,14 +12,13 @@ class TetrisGame : Game
     GameWorld gameWorld;
     Texture2D emptyCell;
     TetrisGrid tetrisGrid;
+    int[][] allBlocks = new int[10][];
     int score, level;
 
     /// <summary>
     /// A static reference to the ContentManager object, used for loading assets.
     /// </summary>
     public static ContentManager ContentManager { get; private set; }
-    test
-    
 
     /// <summary>
     /// A static reference to the width and height of the screen.
@@ -62,39 +62,67 @@ class TetrisGame : Game
         gameWorld = new GameWorld();
         gameWorld.Reset();
     }
-
-    public static BitArray generateBlock(short blockCode)
+    public static int[][] generateForm(int form, int x, int y)
+    {
+        switch (form)
+        {
+            case 0:
+                return new int[][] { new int[] { 0 + x, 0 + y }, new int[]{ 0 + x, 1 + y }, new int[]{ 0 + x, 2 + y }, new int[]{ 0 + x, 3 + y } }; //vierblokkige staaf
+            default:
+                return new int[][] { new int[] { 0 + x, 0 + y }, new int[] { 1 + x, 0 + y }, new int[] { 0 + x, 1 + y }, new int[] { 1 + x, 1 + y } }; //vierkant blok
+        }
+            
+    }
+   /* public static BitArray GenerateBlock(short blockCode)
     {
         byte[] bytes = BitConverter.GetBytes(blockCode);
         System.Collections.BitArray arrBit = new BitArray(bytes);
         return arrBit;
     }
 
-    public static bool getBit(BitArray bitArr, int pos)
+    public static bool GetBit(BitArray bitArr, int pos)
     {
         return bitArr.Get(pos);
-    }
+    } */
 
-    public static int[,] ConvertBAToDrawableArray(int startx, int starty, BitArray BA, Texture2D emptyCell)
+    /*public static int[][] ConvertBAToDrawableArray(int startx, int starty, BitArray BA, Texture2D emptyCell)
     {
-        int[,] arr = new int[12, 12];
+        int[][] jaggedArr = new int[12][];
         int i = 0;
-        int j = 0;
         for(int x = startx; x < startx + 3*emptyCell.Width; startx += emptyCell.Width)
         {
-            i++;
             for(int y = starty; y < starty + 4*emptyCell.Height; starty += emptyCell.Height)
             {
-                j++;
-                if(getBit(BA, (i*3 + j)))
+                if(GetBit(BA, (i)))
                 {
-                    //code
+                    jaggedArr[i] = new int[2] { x, y };
                 }
+                i++;
             }
         }
-        return null;
-    }
+        return jaggedArr;
+    } */
+    /*public static bool[,] ConvertBAToDrawableArray(int startx, int starty, BitArray BA, Texture2D emptyCell)
+{
 
+    bool[,] jaggedArr = new bool[startx/emptyCell.Width + 3, starty/emptyCell.Height + 4];
+    int i = 0;
+    for(int x = startx; x < startx + 3*emptyCell.Width; x += emptyCell.Width)
+    {
+            
+        for(int y = starty; y < starty + 4*emptyCell.Height; y += emptyCell.Height)
+        {
+                
+            if(GetBit(BA, (i)))
+            {
+                    jaggedArr[x / emptyCell.Width, y / emptyCell.Height] = true;
+            } 
+            i++;
+        }
+    }
+    return jaggedArr;  
+}  */
+    
     protected override void Update(GameTime gameTime)
     {
         if (inputHelper.KeyPressed(Microsoft.Xna.Framework.Input.Keys.Escape))
@@ -110,13 +138,25 @@ class TetrisGame : Game
     {
         GraphicsDevice.Clear(Color.White);
         spriteBatch.Begin();
-        for (int x = 0; x < tetrisGrid.Width * emptyCell.Width; x += emptyCell.Width)
+        //bool[,] arr = ConvertBAToDrawableArray(0, 0, GenerateBlock(3456), emptyCell);
+        for (int x = 0; x < tetrisGrid.Width; x++)
         {
-            for (int y = 0; y < tetrisGrid.Height * emptyCell.Height; y += emptyCell.Height)
+            for (int y = 0; y < tetrisGrid.Height; y++)
             {
-             spriteBatch.Draw(emptyCell, new Vector2(x, y), Color.White);
+                {
+                    spriteBatch.Draw(emptyCell, new Vector2(x * emptyCell.Width, y * emptyCell.Height), Color.White);
+                }
             }
         }
+        int[][] form = generateForm(0, 0, 0);
+
+        foreach(int[] coord in form)
+        {
+            spriteBatch.Draw(emptyCell, new Vector2(coord[0] * emptyCell.Width, coord[1] * emptyCell.Height), Color.Yellow);
+        }
+
+
+        //spriteBatch.Draw(emptyCell, new Vector2(0, 0), Color.Yellow);
         spriteBatch.End(); 
         gameWorld.Draw(gameTime, spriteBatch);
     }
